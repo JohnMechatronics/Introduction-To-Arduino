002cb632dfa3eba8bc66c5fe47b08c837d56d9c8
JohnMechatronics <32952197+JohnMechatronics@users.noreply.github.com>
2017-11-10 12:29:27 +0000

    Add files via upload

---
+++ Button-Piezo-Beep.ino
@@ -0,0 +1,43 @@
+
+/*
+  Button and buzzer
+
+  Beeps a piezo on pin 9 when when pressing a pushbutton attached to pin 2.
+
+  The circuit:
+  - piezo buzzer attached from pin 9 to ground
+  - pushbutton attached to pin 2 from +5V
+  - 10K resistor attached to pin 2 from ground
+
+  john.wild@rca.ac.uk
+  
+*/
+
+
+
+const int buttonPin = 2;     // the number of the pushbutton pin
+
+
+// variables will change:
+int buttonState = 0;         // variable for reading the pushbutton status
+
+void setup() {
+  // declare pin 9 to be an output:
+  pinMode(9, OUTPUT);
+  // initialize the pushbutton pin as an input:
+  pinMode(buttonPin, INPUT);
+}
+
+void loop() {
+  // read the state of the pushbutton value:
+  buttonState = digitalRead(buttonPin);
+
+  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+  if (buttonState == HIGH) {
+    // make the buzzer beep:
+    analogWrite(9, 150);     
+  } else {
+    // turn off the buzzer
+    analogWrite(9, 0);       // 0 turns it off
+  }
+}
